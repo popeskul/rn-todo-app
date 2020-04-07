86486dd9071af6a026f704ea88657055f898198c
Popeskul Pavlo <peterjj91@gmail.com>
2020-04-07 19:31:48 +0300

    Validation on empty input

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import { View, StyleSheet, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, TextInput, Button, Alert } from 'react-native';
 
 export const AddTodo = ({ onSubmit }) => {
+  const [value, setValue] = useState('');
+
   const pressHandler = () => {
-    onSubmit('test todo');
+    if (value.trim()) {
+      onSubmit(value);
+      setValue('');
+    } else {
+      Alert.alert('Please type text');
+    }
   };
 
   return (
     <View style={styles.block}>
-      <TextInput style={styles.input} />
+      <TextInput
+        style={styles.input}
+        onChangeText={setValue}
+        value={value}
+        placeholder='Type text'
+      />
       <Button title='Add' style={styles.button} onPress={pressHandler} />
     </View>
   );
