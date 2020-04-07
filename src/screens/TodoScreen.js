34168d506a0add23223c38868b0fbe55e7da1c77
Popeskul Pavlo <peterjj91@gmail.com>
2020-04-07 23:34:28 +0300

    Theme constant

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
+import { THEME } from '../theme';
 
 export const TodoScreen = ({ goBack, todo }) => {
   return (
     <View>
       <Text>{todo.title}</Text>
-      <Button title='back' onPress={goBack} />
+      <View style={styles.buttons}>
+        <View style={styles.button}>
+          <Button title='Back' color={THEME.GREY_COLOR} onPress={goBack} />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title='Delete'
+            color={THEME.DANGER_COLOR}
+            onPress={() => console.log('Remove', todo.id)}
+          />
+        </View>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  button: {
+    width: '40%',
+  },
+});
---
+++ src/theme.js
@@ -0,0 +1,5 @@
+export const THEME = {
+  DANGER_COLOR: '#757575',
+  GREY_COLOR: '#e53935',
+  MAIN_COLOR: '#3949ab',
+};
