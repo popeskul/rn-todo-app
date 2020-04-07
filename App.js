79fbc5b2dff854d3120c4424388a300ca3e3d604
Popeskul Pavlo <peterjj91@gmail.com>
2020-04-07 21:26:32 +0300

    Render list todos by FlatList

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 import { Navbar } from './src/Navbar';
 import { AddTodo } from './src/AddTodo';
 import { Todo } from './src/Todo';
 
 export default function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    { id: 1, title: 'Some text Some text Some text' },
+    { id: 2, title: 'Some text Some text Some text' },
+    { id: 3, title: 'Some text Some text Some text' },
+    { id: 4, title: 'Some text Some text Some text' },
+    { id: 5, title: 'Some text Some text Some text' },
+    { id: 6, title: 'Some text Some text Some text' },
+    { id: 7, title: 'Some text Some text Some text' },
+    { id: 8, title: 'Some text Some text Some text' },
+    { id: 9, title: 'Some text Some text Some text' },
+    { id: 10, title: 'Some text Some text Some text' },
+  ]);
 
   const addTodo = (title) => {
     const newTodo = {
@@ -23,11 +34,11 @@ export default function App() {
       <View style={styles.container}>
         <AddTodo onSubmit={addTodo} />
 
-        <View>
-          {todos.map((todo) => (
-            <Todo key={todo.id} todo={todo} />
-          ))}
-        </View>
+        <FlatList
+          data={todos}
+          renderItem={({ item }) => <Todo todo={item} />}
+          keyExtractor={(item) => item.id.toString()}
+        />
       </View>
     </View>
   );
