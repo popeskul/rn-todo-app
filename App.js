8b5b740388927f42ed5b98a9f567098d1cc61764
Popeskul Pavlo <peterjj91@gmail.com>
2020-04-07 19:11:55 +0300

    Todo, Navbar, AddTodo

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { Navbar } from './src/Navbar';
+import { AddTodo } from './src/AddTodo';
+import { Todo } from './src/Todo';
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (title) => {
+    const newTodo = {
+      id: Date.now().toString(),
+      title,
+    };
+
+    setTodos((prev) => [...prev, newTodo]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Navbar title='Hello Pasha' />
+
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+
+        <View>
+          {todos.map((todo) => (
+            <Todo key={todo.id} todo={todo} />
+          ))}
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 30,
   },
 });
