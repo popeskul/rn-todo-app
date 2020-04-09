b4ecfbe728a47510c4c431082da07c770d91f6d5
Popeskul Pavlo <peterjj91@gmail.com>
2020-04-09 12:20:51 +0300

    Adaptive styles

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
 import { FontAwesome, AntDesign } from '@expo/vector-icons';
 import { AppCard } from '../components/UI/AppCard';
 import { EditModal } from '../components/EditModal';
@@ -60,7 +60,8 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   button: {
-    width: '40%',
+    // width: Dimensions.get('window').width / 3,
+    width: Dimensions.get('window').width / 3 > 400 ? 150 : 100,
   },
   title: {
     fontSize: 20,
