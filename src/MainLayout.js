03c9f42fcfcb338a430cc4738973233610065c6a
Popeskul Pavlo <peterjj91@gmail.com>
2020-04-10 10:50:17 +0300

    Change screen

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -1,24 +1,17 @@
-import React, { useState, useContext } from 'react';
-import { Alert } from 'react-native';
+import React, { useContext } from 'react';
+// import { Alert } from 'react-native';
 import { View, StyleSheet } from 'react-native';
 import { Navbar } from './components/Navbar';
 import { MainScreen } from './screens/MainScreen';
 import { TodoScreen } from './screens/TodoScreen';
 import { TodoContext } from './context/todo/todoContext';
 import { THEME } from './theme';
+import { ScreenContext } from './context/screen/screenContext';
 
 export const MainLayout = () => {
   const { todos, removeTodo, updateTodo, addTodo } = useContext(TodoContext);
-  const [todoId, setTodoId] = useState(null);
-
-  // const addTodo = (title) => {
-  //   const newTodo = {
-  //     id: Date.now().toString(),
-  //     title,
-  //   };
-
-  //   setTodos((prev) => [...prev, newTodo]);
-  // };
+  const { todoId, changeScreen } = useContext(ScreenContext);
+  // const [todoId, setTodoId] = useState(null);
 
   // const removeTodo = (id) => {
   //   const todo = todos.find((todo) => todo.id === id);
@@ -44,23 +37,12 @@ export const MainLayout = () => {
   //   );
   // };
 
-  // const updateTodo = (id, title) => {
-  //   setTodos((prev) =>
-  //     prev.map((todo) => {
-  //       if (todo.id === id) {
-  //         todo.title = title;
-  //       }
-  //       return todo;
-  //     })
-  //   );
-  // };
-
   let content = (
     <MainScreen
       todos={todos}
       addTodo={addTodo}
       removeTodo={removeTodo}
-      openTodo={setTodoId}
+      openTodo={changeScreen}
     />
   );
 
@@ -71,7 +53,7 @@ export const MainLayout = () => {
       <TodoScreen
         onRemove={removeTodo}
         todo={selectedTodo}
-        goBack={() => setTodoId(null)}
+        goBack={() => changeScreen(null)}
         onSave={updateTodo}
       />
     );
--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -1,13 +1,18 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useContext } from 'react';
 import { TodoContext } from './todoContext';
 import { todoReducer } from './todoReducer';
 import { initialState } from './initialState';
 import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types';
+import { ScreenContext } from '../screen/screenContext';
 
 export const TodoState = ({ children }) => {
   const [state, dispatch] = useReducer(todoReducer, initialState);
+  const { changeScreen } = useContext(ScreenContext);
 
-  const addTodo = (title) => dispatch({ type: ADD_TODO, title });
+  const addTodo = (title) => {
+    changeScreen(null);
+    dispatch({ type: ADD_TODO, title });
+  };
 
   const removeTodo = (id) => dispatch({ tyep: REMOVE_TODO, id });
 
