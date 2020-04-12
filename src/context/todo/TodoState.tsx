55cebd6adce6ddb18bcce8220cc095d40c0c422f
Popeskul Pavlo <peterjj91@gmail.com>
2020-04-12 19:00:51 +0300

    Delete todo

--- src/context/todo/TodoState.tsx
+++ src/context/todo/TodoState.tsx
@@ -52,8 +52,15 @@ export const TodoState: React.FC<Props> = ({ children }) => {
         {
           text: 'Delete',
           style: 'destructive',
-          onPress: () => {
+          onPress: async () => {
             changeScreen(null);
+            await fetch(
+              `https://rn-todo-app-bd3c7.firebaseio.com/todos/${id}.json`,
+              {
+                method: 'DELETE',
+                headers: { 'Content-Type': 'application/json' },
+              }
+            );
             dispatch({ type: REMOVE_TODO, id });
           },
         },
